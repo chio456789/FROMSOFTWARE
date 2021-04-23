3677a65ffbd076f8b17058bde674e98f271ef035
Walter Rojas Montan <wal_tico02@hotmail.com>
2021-04-23 10:29:56 -0400

    mejorado

--- WpfApp1/WpfApp1/PAGES/ProductosAdm.xaml.cs
+++ WpfApp1/WpfApp1/PAGES/ProductosAdm.xaml.cs
@@ -48,7 +48,10 @@ namespace WpfApp1.Pages
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            int id = (int)((Button)sender).CommandParameter;
+
             AgregarEditarProductoNuevo ventanaProducto = new AgregarEditarProductoNuevo();
+           
             ventanaProducto.Show();
         }
 
@@ -59,10 +62,13 @@ namespace WpfApp1.Pages
             int id =(int)((Button)sender).CommandParameter;
             using (Model.puntoDeVentaDB_testEntities contexto = new Model.puntoDeVentaDB_testEntities())
             {
-                contexto.Entry(id).State = System.Data.Entity.EntityState.Deleted;
+                var produto = contexto.productos.Find(id);
+                contexto.productos.Remove(produto);
                 contexto.SaveChanges();
             }
+            actualizar();
         }
+
 
         public class ProductViewModel
             {
@@ -72,13 +78,13 @@ namespace WpfApp1.Pages
             public decimal PrecioProducto { get; set; }
             public decimal CostoProducto { get; set; }
             //public bool DisponibilidadProducto { get; set; }
-            public int Categoria { get; set; }
+           // public int Categoria { get; set; }
         }
 
         private void BotonEditar(object sender, RoutedEventArgs e)
         {
-            int id = (int)((Button)sender).CommandParameter;
-            AgregarEditarProductoNuevo Editarproduct = new AgregarEditarProductoNuevo(id);
+            int tol = (int)((Button)sender).CommandParameter;
+            AgregarEditarProductoNuevo Editarproduct = new AgregarEditarProductoNuevo(tol);           
             Editarproduct.Show();
             actualizar();
         }
