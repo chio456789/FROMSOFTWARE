de03216b4b04e67f0cc242f5ac2edbcf6b8ebde6
Daryl Lawless <h666_metal@hotmail.com>
2021-04-25 17:27:14 -0400

    Ordenes en proceso

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -7,11 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MessageBox = System.Windows.Forms.MessageBox;
 
 namespace WpfApp1
 {
@@ -31,8 +33,14 @@ namespace WpfApp1
         private void BtnCerrarSesion_Click(object sender, RoutedEventArgs e)
         {
             Login log = new Login();
-            this.Close();
-            log.Show();
+            DialogResult DR = MessageBox.Show("¿Cerrar la sesión actual?", "Serrar sesión", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (DR == System.Windows.Forms.DialogResult.Yes)
+            {
+                this.Close();
+                log.Show();
+            }
+            
+            
         }
 
         private void BtnMainWinMenu_Click(object sender, RoutedEventArgs e)
